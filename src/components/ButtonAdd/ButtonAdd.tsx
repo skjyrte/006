822705dfaa8f9d1fc52c2c313232a7da4f9a5ede
unknown <wasylewiczkrzysztof@gmail.com>
2023-10-14 22:59:58 +0200

    add button loader

--- src/components/ButtonAdd/ButtonAdd.tsx
+++ src/components/ButtonAdd/ButtonAdd.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import "./ButtonAdd.css";
+import MoonLoader from "react-spinners/MoonLoader";
+import { CSSProperties } from "react";
 
 export default function ButtonAdd(props: any) {
-  const { onClick } = props;
+  const { onClick, isEntryAdding } = props;
+
+  const override: CSSProperties = {
+    display: "block",
+    borderColor: "red",
+    position: "absolute",
+    /*     transform: "translate(-50%, -50%)", */
+    zIndex: "10",
+  };
+  const color = "yellow";
+
   return (
     <>
-      <div className="button-delete-container">
+      <div className="button-add-container">
         <button className="button-add" onClick={onClick}>
           <img className="button-add" src="./icon-add.svg" alt="icon add"></img>
         </button>
+        <MoonLoader
+          color={color}
+          cssOverride={override}
+          loading={isEntryAdding}
+          size={40}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        ></MoonLoader>
       </div>
     </>
   );
--- src/components/InputBar/InputBar.tsx
+++ src/components/InputBar/InputBar.tsx
@@ -1,9 +1,23 @@
 import "./InputBar.css";
 import ButtonAdd from "../ButtonAdd/ButtonAdd";
 import CharCounter from "../CharCounter/CharCounter";
+import { useState } from "react";
 
 export default function InputBar(props: any) {
   const { onChange, onClick, inputValue } = props;
+  const [isEntryAdding, setIsEntryAdding] = useState<boolean>(false);
+
+  async function handleEntryAdd() {
+    try {
+      setIsEntryAdding(() => true);
+      await onClick();
+    } catch {
+      throw new Error("error adding todo");
+    } finally {
+      setIsEntryAdding(() => false);
+    }
+  }
+
   const currentInputLength = inputValue.length;
   return (
     <div className="input-bar">
@@ -26,7 +40,7 @@ export default function InputBar(props: any) {
         onChange={onChange}
         value={inputValue}
       />
-      <ButtonAdd onClick={onClick} />
+      <ButtonAdd onClick={handleEntryAdd} isEntryAdding={isEntryAdding} />
     </div>
   );
 }
